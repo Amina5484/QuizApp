9c15012c7b31c4052d8dd8111b46b3ed5dfd0918
userName <aminamohammed10004@gmail.com>
2024-05-14 00:25:04 +0300

    quiz_app

--- lib/main.dart
+++ lib/main.dart
@@ -57,33 +57,33 @@ class _QuizPageState extends State<QuizPage> {
       ['Dart', 'Java', 'Python'],
       0,
     ),
-   Question(
-  'What is wireless communication?',
-  [
-    'Communication without the use of physical cables or wires',
-    'Communication using radio waves',
-    'Communication through satellite systems'
-  ],
-  0,
-),
-Question(
-    'What is a mobile operating system?',
-  [
-    ' Software that enables wireless communication between devices',
-    ' A type of mobile phone',
-    ' Software that manages and controls mobile devices',
-  ],
-  2,
-),
-Question(
-  'What is mobile computing?',
-  [
-    'Computing using portable devices like smartphones and tablets',
-    'Computing on the move without a fixed location',
-    'Computing using cloud-based services'
-  ],
-  0,
-),
+    Question(
+      'What is wireless communication?',
+      [
+        'Communication without the use of physical cables or wires',
+        'Communication using radio waves',
+        'Communication through satellite systems'
+      ],
+      0,
+    ),
+    Question(
+      'What is a mobile operating system?',
+      [
+        ' Software that enables wireless communication between devices',
+        ' A type of mobile phone',
+        ' Software that manages and controls mobile devices',
+      ],
+      2,
+    ),
+    Question(
+      'What is mobile computing?',
+      [
+        'Computing using portable devices like smartphones and tablets',
+        'Computing on the move without a fixed location',
+        'Computing using cloud-based services'
+      ],
+      0,
+    ),
   ];
 
   int currentQuestionIndex = 0;
@@ -96,7 +96,8 @@ Question(
       selectedAnswers[currentQuestionIndex] = selectedAnswerIndex;
       correctAnswers.add(questions[currentQuestionIndex].correctAnswerIndex);
 
-      if (selectedAnswerIndex == questions[currentQuestionIndex].correctAnswerIndex) {
+      if (selectedAnswerIndex ==
+          questions[currentQuestionIndex].correctAnswerIndex) {
         score++;
       }
 
@@ -105,7 +106,13 @@ Question(
       } else {
         Navigator.pushReplacement(
           context,
-          MaterialPageRoute(builder: (context) => ResultPage(score: score, totalQuestions: questions.length, correctAnswers: correctAnswers, selectedAnswers: selectedAnswers, questions: questions)),
+          MaterialPageRoute(
+              builder: (context) => ResultPage(
+                  score: score,
+                  totalQuestions: questions.length,
+                  correctAnswers: correctAnswers,
+                  selectedAnswers: selectedAnswers,
+                  questions: questions)),
         );
       }
     });
@@ -146,37 +153,63 @@ Question(
           ),
           Column(
             children: [
-              for (int i = 0; i < questions[currentQuestionIndex].choices.length; i++)
-                RadioListTile<int>(
-                  title: Text(
-                    questions[currentQuestionIndex].choices[i],
-                    style: const TextStyle(color: Colors.white),
+              for (int i = 0;
+                  i < questions[currentQuestionIndex].choices.length;
+                  i++)
+                SizedBox(
+                  width: 800, // Adjust the width as needed
+                  height: 60,
+                  child: Container(
+                    margin: const EdgeInsets.symmetric(vertical: 4),
+                    decoration: BoxDecoration(
+                      color: Colors.deepPurple,
+                      borderRadius: BorderRadius.circular(8),
+                    ),
+                    child: RadioListTile<int>(
+                      title: Container(
+                        alignment: Alignment.center,
+                        child: Text(
+                          questions[currentQuestionIndex].choices[i],
+                          style: const TextStyle(color: Colors.white),
+                        ),
+                      ),
+                      value: i,
+                      groupValue: selectedAnswers[currentQuestionIndex],
+                      onChanged: (int? value) {
+                        setState(() {
+                          selectedAnswers[currentQuestionIndex] = value!;
+                        });
+                      },
+                      controlAffinity: ListTileControlAffinity.trailing,
+                      tileColor: selectedAnswers[currentQuestionIndex] == i
+                          ? Colors.deepPurple
+                          : null,
+                      selectedTileColor: Colors.deepPurple,
+                    ),
                   ),
-                  value: i,
-                  groupValue: selectedAnswers[currentQuestionIndex],
-                  onChanged: (int? value) {
-                    setState(() {
-                      selectedAnswers[currentQuestionIndex] = value!;
-                    });
-                  },
                 ),
             ],
           ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              ElevatedButton(
-                onPressed: goBack,
-                child: const Text('Back'),
-              ),
-              const SizedBox(width: 16),
-              ElevatedButton(
-                onPressed: () {
-                  checkAnswer(selectedAnswers[currentQuestionIndex]);
-                },
-                child: Text(currentQuestionIndex < questions.length - 1 ? 'Next' : 'Submit'),
-              ),
-            ],
+          Padding(
+            padding: const EdgeInsets.only(top: 20),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                ElevatedButton(
+                  onPressed: goBack,
+                  child: const Text('Back'),
+                ),
+                const SizedBox(width: 20),
+                ElevatedButton(
+                  onPressed: () {
+                    checkAnswer(selectedAnswers[currentQuestionIndex]);
+                  },
+                  child: Text(currentQuestionIndex < questions.length - 1
+                      ? 'Next'
+                      : 'Submit'),
+                ),
+              ],
+            ),
           ),
         ],
       ),
@@ -191,7 +224,14 @@ class ResultPage extends StatelessWidget {
   final List<int> selectedAnswers;
   final List<Question> questions;
 
-  const ResultPage({Key? key, required this.score, required this.totalQuestions, required this.correctAnswers, required this.selectedAnswers, required this.questions}) : super(key: key);
+  const ResultPage({
+    Key? key,
+    required this.score,
+    required this.totalQuestions,
+    required this.correctAnswers,
+    required this.selectedAnswers,
+    required this.questions,
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -200,7 +240,8 @@ class ResultPage extends StatelessWidget {
       appBar: AppBar(
         title: const Text('Quiz Result'),
       ),
-      body: Center(
+      body: SingleChildScrollView(
+        padding: const EdgeInsets.all(16),
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
@@ -213,13 +254,39 @@ class ResultPage extends StatelessWidget {
               style: const TextStyle(fontSize: 18, color: Colors.white),
             ),
             for (int i = 0; i < totalQuestions; i++)
-              Text(
-                'Question ${i + 1}: ${correctAnswers[i] == selectedAnswers[i] ? 'Correct' : 'Incorrect'}, Correct Answer: ${questions[i].choices[questions[i].correctAnswerIndex]}',
-                style: const TextStyle(fontSize: 16, color: Colors.white),
+              Padding(
+                padding: const EdgeInsets.symmetric(vertical: 8),
+                child: Column(
+                  children: [
+                    Text(
+                      'Question ${i + 1}: ${correctAnswers[i] == selectedAnswers[i] ? 'Correct' : 'Incorrect'}',
+                      style: const TextStyle(fontSize: 16, color: Colors.white),
+                    ),
+                    Text(
+                      'Question: ${questions[i].questionText}',
+                      style: const TextStyle(
+                          fontSize: 20,
+                          color: Colors.white,
+                          fontWeight: FontWeight.bold),
+                    ),
+                    Text(
+                      'Correct Answer: ${questions[i].choices[questions[i].correctAnswerIndex]}',
+                      style: const TextStyle(
+                          fontSize: 16,
+                          color: Colors.white,
+                          fontWeight: FontWeight.bold),
+                    ),
+                    Text(
+                      'Your Answer: ${questions[i].choices[selectedAnswers[i]]}',
+                      style: const TextStyle(fontSize: 16, color: Colors.white),
+                    ),
+                  ],
+                ),
               ),
             ElevatedButton(
               onPressed: () {
-                Navigator.popUntil(context, ModalRoute.withName(Navigator.defaultRouteName));
+                Navigator.popUntil(
+                    context, ModalRoute.withName(Navigator.defaultRouteName));
               },
               child: const Text('Restart Quiz'),
             ),
